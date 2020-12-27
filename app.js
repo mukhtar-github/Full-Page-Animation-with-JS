d5f4c33062a6282486073ab29cbd4967551848bf
Mukhtar <mukhtartanimugarba@gmail.com>
2020-12-27 07:28:04 +0100

    Update app & index files

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 new fullpage('#fullpage', {
     autoScrolling: true,
-    navigation: true
+    navigation: true,
+    onLeave: (origin, destination, direction) => {
+        const section = destination.item;
+        const title = section.querySelector('hi');
+        const tl = gsap.timeline();
+        tl.fromTo(title,0.5, {y: '50', opacity: 0}, {y: 0, opacity: 1})
+    }
 });
