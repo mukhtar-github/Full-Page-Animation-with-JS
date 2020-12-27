ecf038ae6d35dc7cc83d6bcbae68a187e6bea856
Mukhtar <mukhtartanimugarba@gmail.com>
2020-12-27 08:28:57 +0100

    Update app file

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
-    onLeave: (origin, destination, direction) => {
+    onLeave: (origin,destination,direction) => {
         const section = destination.item;
-        const title = section.querySelector('hi');
-        const tl = gsap.timeline();
-        tl.fromTo(title,0.5, {y: '50', opacity: 0}, {y: 0, opacity: 1})
+        const title = section.querySelector('h1');
+        const tl = gsap.timeline({ delay: 0.5 });
+        tl.fromTo(title,0.5, {y: 50, opacity: 0}, {y: 0, opacity: 1});
     }
 });
